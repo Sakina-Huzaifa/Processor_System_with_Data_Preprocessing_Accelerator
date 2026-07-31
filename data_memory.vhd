4610a997e081631eb8151c61b90be0ae2b9d1761
Eva Acharya <250652058+penguino-saur@users.noreply.github.com>
2026-07-31 15:20:42 +1000

    Updated data-memory.vhd
    - Extended the data

--- data_memory.vhd
+++ data_memory.vhd
@@ -28,50 +28,74 @@ entity data_memory is
            clk          : in  std_logic;
            write_enable : in  std_logic;
            write_data   : in  std_logic_vector(15 downto 0);
-           addr_in      : in  std_logic_vector(3 downto 0);
-           data_out     : out std_logic_vector(15 downto 0) );
+           addr_in      : in  std_logic_vector(9 downto 0);
+           data_out     : out std_logic_vector(15 downto 0);
+           
+           cop_busy         : in std_logic;
+           cop_write_enable : in std_logic;
+           cop_write_data   : in std_logic_vector(15 downto 0);
+           cop_addr_in      : in std_logic_vector(9 downto 0);
+           
+           transfer_busy         : in std_logic;
+           transfer_write_enable : in std_logic;
+           transfer_write_data   : in std_logic_vector(15 downto 0);
+           transfer_addr_in      : in std_logic_vector(9 downto 0) );
 end data_memory;
 
 architecture behavioral of data_memory is
 
-type mem_array is array(0 to 15) of std_logic_vector(15 downto 0);
+type mem_array is array(0 to 1023) of std_logic_vector(15 downto 0);
 signal sig_data_mem : mem_array;
 
+signal selected_addr           : std_logic_vector(9 downto 0);
+signal selected_write_enable   : std_logic;
+signal selected_write_data     : std_logic_vector(15 downto 0);
+
 begin
-    mem_process: process ( reset,
+
+select_inputs: process(cop_busy, cop_addr_in, cop_write_enable, cop_write_data,
+                        transfer_busy, transfer_write_enable, transfer_addr_in, 
+                        transfer_write_data, addr_in, write_enable, write_data)
+begin 
+    if cop_busy = '1' then
+        selected_addr           <= cop_addr_in;
+        selected_write_enable   <= cop_write_enable;
+        selected_write_data     <= cop_write_data;
+        
+    elsif transfer_busy = '1' then 
+        selected_addr           <= transfer_addr_in;
+        selected_write_enable   <= transfer_write_enable;
+        selected_write_data     <= transfer_write_data;
+    
+    else    
+        selected_addr           <= addr_in;
+        selected_write_enable   <= write_enable;
+        selected_write_data     <= write_data;
+    
+    end if;
+end process;
+    
+mem_process: process ( reset,
                            clk,
-                           write_enable,
-                           write_data,
-                           addr_in ) is
+                           selected_addr,
+                           selected_write_enable,
+                           selected_write_data ) is
   
     variable var_data_mem : mem_array;
     variable var_addr     : integer;
   
     begin
-        var_addr := conv_integer(addr_in);
+        var_addr := conv_integer(selected_addr);
         
         if (reset = '1') then
             -- initial values of the data memory : reset to zero 
-            var_data_mem(0)  := X"0005";
-            var_data_mem(1)  := X"0008";
-            var_data_mem(2)  := X"0000";
-            var_data_mem(3)  := X"0000";
-            var_data_mem(4)  := X"0000";
-            var_data_mem(5)  := X"0000";
-            var_data_mem(6)  := X"0000";
-            var_data_mem(7)  := X"0000";
-            var_data_mem(8)  := X"0000";
-            var_data_mem(9)  := X"0000";
-            var_data_mem(10) := X"0000";
-            var_data_mem(11) := X"0000";
-            var_data_mem(12) := X"0000";
-            var_data_mem(13) := X"0000";
-            var_data_mem(14) := X"0000";
-            var_data_mem(15) := X"0000";
+            for i in 0 to 1023 loop 
+                var_data_mem(i) := X"0000";
+            end loop;
 
-        elsif (falling_edge(clk) and write_enable = '1') then
+        elsif (falling_edge(clk) and selected_write_enable = '1') then
             -- memory writes on the falling clock edge
-            var_data_mem(var_addr) := write_data;
+            var_data_mem(var_addr) := selected_write_data;
         end if;
        
         -- continuous read of the memory location given by var_addr 
